041885dc4341941907b1eb21889627ab1faa3517
blckshrk <alexand.bonhom@gmail.com>
2014-01-21 19:07:26 +0100

    Implementation of SegregationSMA

---
+++ src/segregation/SegregationSMA.py
@@ -0,0 +1,26 @@
+'''
+Created on 21 janv. 2014
+
+@author: Alexandre Bonhomme
+'''
+from core.SMA import SMA
+from segregation.ColorAgent import ColorAgent
+
+
+class SegregationSMA(SMA):
+
+    def __init__(self, cols, rows, satisfactionThreshold, waitingTimeMillis = 100, logFilename = None):
+        SMA.__init__(self, cols, rows, waitingTimeMillis, logFilename)
+
+        self.satisfactionThreshold = satisfactionThreshold
+
+    def initRedAgents(self, nRedAgents):
+        self._initColorAgents(nRedAgents, 'red')
+
+    def initBlueAgents(self, nRedAgents):
+        self._initColorAgents(nRedAgents, 'blue')
+
+    def _initColorAgents(self, nColorAgent, color):
+        for _ in xrange(0, nColorAgent):
+            x, y = self.env.randomEmptyPosition()
+            self.addAgent(ColorAgent(x, y, self, color, self.satisfactionThreshold))
