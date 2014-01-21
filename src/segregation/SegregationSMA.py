4feaad0f08a98bb9f86bd0defc09d64a25a8e558
Clem-me <dufour.clement@gmail.com>
2014-01-21 20:24:28 +0100

    Add computeGlobalSatisfaction andcomputeUnsatisfaction methods on SegregationSMA

--- src/segregation/SegregationSMA.py
+++ src/segregation/SegregationSMA.py
@@ -24,3 +24,32 @@ class SegregationSMA(SMA):
         for _ in xrange(0, nColorAgent):
             x, y = self.env.randomEmptyPosition()
             self.addAgent(ColorAgent(x, y, self, color, self.satisfactionThreshold))
+
+    '''
+    Return the average satisfaction of agents
+    '''
+    def computeGlobalSatisfaction(self):
+        satisfations = 0.0
+        
+        if len(self.agentsList) <= 0:
+            raise ValueError("Empty agents list")
+        
+        for agent in self.agentsList:
+            satisfations += agent.satisfaction
+            
+        return satisfations / len(self.agentsList)
+    
+    '''
+    Return the percent of agents who aren't satisfied
+    '''
+    def computeUnsatisfaction(self):
+        unsatisfaction = 0
+        
+        if len(self.agentsList) <= 0:
+            raise ValueError("Empty agents list")
+        
+        for agent in self.agentsList:
+            if agent.satisfaction < self.satisfactionThreshold:
+                unsatisfaction += 1
+        
+        return unsatisfaction / len(self.agentsList)
