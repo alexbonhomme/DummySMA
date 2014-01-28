f35dd107fa348fdd1ff73780fafcd8dd20d584c3
blckshrk <alexand.bonhom@gmail.com>
2014-01-28 23:13:16 +0100

    Partial implementation of a maze builder agent (juste init a grid of walls for now)

--- src/explorer/ExplorerSMA.py
+++ src/explorer/ExplorerSMA.py
@@ -13,14 +13,7 @@ class ExplorerSMA(SMA):
         SMA.__init__(self, cols, rows, logFilename)
 
     def initWalls(self):
-        # TODO
-        for x in xrange(self.env.rows):
-            self.addAgent(WallAgent(x, 0, self))
-            self.addAgent(WallAgent(x, self.env.cols - 1, self))
-
-        for y in xrange(self.env.cols):
-            self.addAgent(WallAgent(0, y, self))
-            self.addAgent(WallAgent(self.env.rows - 1, y, self))
+        pass
 
     def initExplorers(self, nExplorers):
         pass
