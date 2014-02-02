790970842b573582eb4a3941f07881358387b0ad
blckshrk <alexand.bonhom@gmail.com>
2014-02-02 22:58:39 +0100

    Adding the Dijkstra's grid computation in action() method of ExplorerAgent

--- src/explorer/agents/ExplorerAgent.py
+++ src/explorer/agents/ExplorerAgent.py
@@ -4,15 +4,17 @@ Created on 28 janv. 2014
 @author: Alexandre Bonhomme
 '''
 from core.agents.AgentMovable import AgentMovable
+from core.agents.AgentDijkstra import AgentDijkstra
 
 
-class ExplorerAgent(AgentMovable):
+class ExplorerAgent(AgentMovable, AgentDijkstra):
 
     def __init__(self, x, y, sma, color = 'light blue'):
-        AgentMovable.__init__(self, x, y, sma)
+        AgentMovable.__init__(self, x, y, sma) # could be ignored
+        AgentDijkstra.__init__(self, x, y, sma)
 
         self.color = color
-        self.dijkstraGrid = [[None for _ in xrange(self.sma.env.cols)] for _ in xrange(self.sma.env.rows)]
 
     def action(self):
-        pass
+        # compute the Dijkstra's grid
+        self.computeDijkstraGrid()
