bf3710b458753a0a4db5e81a1a6163d87cc5cd96
blckshrk <alexand.bonhom@gmail.com>
2014-02-02 22:59:59 +0100

    Implementation of drawDijkstraGrid() and calling of these in _draw() to display the grid of each ExplorerAgent (yes, overing of the previous grid if multiple explorers)

--- src/explorer/ExplorerFrame.py
+++ src/explorer/ExplorerFrame.py
@@ -8,6 +8,7 @@ from Tkinter import ALL
 from gui.FrameTk import FrameTk
 from explorer.agents.ExplorerAgent import ExplorerAgent
 from core.agents.AgentWall import AgentWall
+import logging as log
 
 class ExplorerFrame(FrameTk):
 
@@ -30,6 +31,15 @@ class ExplorerFrame(FrameTk):
                                      y + self.BOX_SIZE, \
                                      fill = color)
 
+    def drawDijkstraGrid(self, grid):
+        rows, cols = self.sma.env.rows, self.sma.env.cols
+        for x in xrange(0, rows):
+            for y in xrange(0, cols):
+                element = grid[x][y]
+
+                if element is not None:
+                    self.drawText(x * self.BOX_SIZE, y * self.BOX_SIZE, element, color = 'white')
+
     def _draw(self):
         title = "Explorer - Tick " + str(self.sma.ticksCounter)
         self.main.wm_title(title)
@@ -43,6 +53,8 @@ class ExplorerFrame(FrameTk):
                 element = grid[x][y]
                 if isinstance(element, ExplorerAgent):
                     self.drawExplorer(x * self.BOX_SIZE, y * self.BOX_SIZE, element.color)
+                    self.drawDijkstraGrid(element.dijkstraGrid)
+
                 elif isinstance(element, AgentWall):
                     self.drawWall(x * self.BOX_SIZE, y * self.BOX_SIZE)
 
